2ed754e8c6afbb9811be20fd7fec1dcb4c8499c3
MEE-DRED <m.ebomah@alustudent.com>
2025-02-24 13:25:42 -0800

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        from add_0 import add
-            a = 1
-                b = 2
-                    print("{} + {} = {}".format(a, b, add(a, b)))
+        from add_0 import add  # Import the add function from add_0.py
+
+            a = 1  # Define variable a
+                b = 2  # Define variable b
+                    
+                        result = add(a, b)  # Call the add function with a and b
+                            
+                                print("{} + {} = {}".format(a, b, result))  # Print the formatted result
+
