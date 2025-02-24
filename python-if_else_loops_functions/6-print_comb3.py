61b5d67380b134d33eee742995b83b72dbb15ab9
MEE-DRED <m.ebomah@alustudent.com>
2025-02-24 12:42:48 -0800

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
         for j in range(i + 1, 10):
-                    print("{:02}".format(i) + "{:01}".format(j), end=", " if i < 8 else "\n")
+                    print("{:d}{:d}".format(i, j), end=", " if i != 8 else "\n")
+
