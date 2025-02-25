0aa1f7a02b430dd15bdc3f15353858abd2b7260f
MEE-DRED <m.ebomah@alustudent.com>
2025-02-25 09:37:33 -0800

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-        if idx < 0:
-                    return (None)
-
-                    length = len(my_list)
-
-                        if idx > length - 1:
-                                    return (None)
-
-                                    return (my_list[idx])
+        # Check if idx is negative or out of range
+            if idx < 0 or idx >= len(my_list):
+                        return None
+                        # Return the element at the specified index
+                            return my_list[idx]
