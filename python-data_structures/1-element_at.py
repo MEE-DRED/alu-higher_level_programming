d1b7ff22c2614f9fae920db99dc7b2665a37285f
MEE-DRED <m.ebomah@alustudent.com>
2025-02-25 13:07:54 -0800

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 1-element_at.py
 def element_at(my_list, idx):
-        if idx < 0 or idx >= len(my_list):
-                    return None
-                    else:
-                                return my_list[idx]
+        """Retrieve an element from a list at a given index."""
+            if idx < 0 or idx >= len(my_list):
+                        return None
+                        return my_list[idx]
