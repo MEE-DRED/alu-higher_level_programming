babd4ba5d7ff047149de9d297b1b66594239dd9b
MEE-DRED <m.ebomah@alustudent.com>
2025-02-28 06:10:39 -0800

    11-delete_at.py

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,17 @@
+#!/usr/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+                    
+    # Create a new list by skipping the element at index idx
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+                                                            
+    # Clear the original list and extend it with the new list
+    my_list.clear()
+    my_list.extend(new_list)
+                                                                            
+    return my_list
+                                                                            
