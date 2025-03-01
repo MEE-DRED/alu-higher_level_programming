13848b65f26951a8cbe0478a65bd69b57ff811bd
MEE-DRED <m.ebomah@alustudent.com>
2025-03-01 12:05:44 -0800

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-        import hidden_4
-            for name in dir(hidden_4):
-                        if name[0] != '_' and name[1] != '_':
-                                        print(name)
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
