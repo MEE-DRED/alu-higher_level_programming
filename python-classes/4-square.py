81780f9a37cf1cc275b522e7ec2cefb4eb9822a0
MEE-DRED <m.ebomah@alustudent.com>
2025-03-06 03:42:15 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """Write a class Square that defines a square"""
+ @property
+ def size(self):
+     """Retrieve size"""
+     return self.__size
 
+ @size.setter
+ def size(self, value):
+     """Set size with validation"""
+     if not isinstance(value, int):
+        raise TypeError("size must be an integer")
+     if value < 0:
+         raise ValueError("size must be >= 0")
+     self.__size = value
 
-class Square:
-    """Write a class Square that defines a square"""
+ def area(self):
+     """Return the current square area"""
+     return self.__size ** 2
 
-    def __init__(self, size=0):
-        self.size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self)
-        return self.__size**2
