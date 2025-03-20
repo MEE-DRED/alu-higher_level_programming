23469daacccbe8c28ad40ebc6923ade16ee8b965
MEE-DRED <m.ebomah@alustudent.com>
2025-03-20 14:02:02 -0700

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
-  """
-  import sys
-  import urllib.parse
-  import urllib.request
+import urllib.request
+import urllib.parse
+import sys
 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+                
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+                        
+    with urllib.request.urlopen(url, data=data) as response:
+        print(response.read().decode("utf-8"))
 
-  if __name__ == "__main__":
-      url = sys.argv[1]
-      value = {"email": sys.argv[2]}
-      data = urllib.parse.urlencode(value).encode("ascii")
-
-      request = urllib.request.Request(url, data)
-      with urllib.request.urlopen(request) as response:
-          print(response.read().decode("utf-8"))
